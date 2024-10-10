40dc073299df163848c14488cb24333dd25c724b
qweri0p <71005650+qweri0p@users.noreply.github.com>
2024-10-10 16:19:25 +0200

    complete rewrite of bandcampExtractor
    Now uses pathlib instead of deprecated os library
    slightly worse performance but better user experience
    also really clean code (:

--- bandcampExtractor/README.md
+++ bandcampExtractor/README.md
@@ -1,16 +1,17 @@
 # bandc.py
-Extracts zips downloaded from [bandcamp](https://bandcamp.com). Usually the naming is very annoying, but this tool renames them to something acceptable.
-Instead of `artist - album/artist - album - tracknumber trackname` it's `album/tracknumber trackname`.
-You'll need the ['music-tag' library from PyPi](https://pypi.org/project/music-tag/). Use the nix-shell environment included in the repo, a [python virtual environment](https://docs.python.org/3/library/venv.html) or the [AUR package](https://aur.archlinux.org/packages/python-music-tag).
 
-Bug: When the name of the artist or album is too long it will crash.
-
-Bug: Doesn't work on termux. (somehow)
-
-If it says that the 'temp' directory exists, delete it.
+Extracts zips downloaded from [bandcamp](https://bandcamp.com).
+Usually the naming is very annoying, but this tool renames them to something acceptable.
+Instead of `artist - album/artist - album - tracknumber trackname`
+it's `album/tracknumber trackname`.
+You'll need the ['music-tag' library from PyPi](https://pypi.org/project/music-tag/).
+Use the nix-shell environment included in the repo, a [python virtual environment](https://docs.python.org/3/library/venv.html)
+or the [AUR package](https://aur.archlinux.org/packages/python-music-tag).
 
 ## Optional Arguments
+
 * `-v`/`--verbose` - Lists each track being extracted individually.
 * `-q`/`--quiet` - Do not display any information at all apart from crashes.
-* `-d`/`--dir` - Runs the program in the specified directory.
 * `-c`/`--clean` - Removes zipfile when finished.
+* `-k`/`--keep` - Do not delete temporary files when finished.
+* `-d`/`--dir` - Runs the program in the specified directory.
--- bandcampExtractor/bandc.py
+++ bandcampExtractor/bandc.py
@@ -1,47 +1,97 @@
 #!/bin/env python
-import zipfile, os, argparse
+import zipfile, argparse, shutil, sys
+from pathlib import Path
+
+MUSICFILETYPES = ['.mp3', '.flac', '.ogg', '.aiff', '.wav', '.alac', '.aac']
+
 try: 
     import music_tag
 except ImportError:
     print("Please install the 'music-tag' python package. Check the README for more details.")
-    exit()
+    exit(1)
 
 parser = argparse.ArgumentParser(description='Unzip zipfiles downloaded from Bandcamp.com into directories labelled by album.', epilog="https://github.com/qweri0p/myPyScripts")
 parser.add_argument('-v', '--verbose', help="Display individual items being extracted.", action='store_true', default=False)
 parser.add_argument('-q', '--quiet', help="Do not display any information at all.", action='store_true', default=False)
 parser.add_argument('-c', '--clean', help="Removes zipfile once completed", action='store_true', default=False)
+parser.add_argument('-k', '--keep', help="Keep the temporary extracted files.", action='store_true', default=False)
 parser.add_argument('-d', '--dir', help="Runs the program in the specified directory.", default=".")
 parser.add_argument('zips', metavar="zipfiles", help="Only unzips specified zipfiles.", nargs='*')
 args = parser.parse_args()
 
-files = os.listdir(args.dir)
 albums = []
 
+# if the zips argument is parsed, don't do the fs scanning
 if args.zips:
     for i in args.zips:
         albums.append(i.split(".zip")[0])
 else:
+    filepath = Path(args.dir)
+    files = sorted([str(file) for file in filepath.iterdir() if file.is_file() and file.suffix == '.zip'])
     for i in files:
-        if i.endswith(".zip"):
-            albums.append(i.split(".zip")[0])
+        albums.append(i.split(".zip")[0])
 
+# start actual process
 for i in albums:
-    with zipfile.ZipFile(args.dir + "/" + i + ".zip", "r") as zip_ref:
-        os.mkdir(args.dir + "/" + 'temp')
-        templist = zip_ref.namelist()
-        if args.quiet == False:
-            print("Starting: " + i)
-        for j in templist:
-            if args.verbose ==True:
-                print("Extracting " + j)
-            zip_ref.extract(j, args.dir + "/" + 'temp')
-            if j.endswith(".mp3") or j.endswith(".wav") or j.endswith(".flac") or j.endswith(".ogg") or j.endswith(".aac") or j.endswith(".alac") or j.endswith(".aiff"):
-                musicFile = music_tag.load_file(os.path.join(".", args.dir + "/" + 'temp',j))
-                album = str(musicFile['album'])
-                artist = str(musicFile['artist'])
-                os.rename(os.path.join(args.dir + "/" + 'temp',j),os.path.join(args.dir + "/" + 'temp',j[len(artist)+len(album)+6:]))
-        if args.quiet == False:
-            print("Finished: " +artist + " - " + album)
-        if args.clean == True:
-            os.remove(args.dir + "/" + i + ".zip")
-        os.rename(args.dir + "/" + 'temp', args.dir + "/" + album)
+    try:
+        zip_ref = zipfile.ZipFile(args.dir + "/" + i + ".zip", "r")
+    except zipfile.BadZipFile:
+        print(f'ERROR: Zipfile {i+'.zip'} is corrupt. Please redownload the music from bandcamp', file=sys.stderr)
+        exit(1)
+
+    if args.quiet == False:
+        print(f'Extracting: {i+'.zip'}')
+
+    # from the name of the zipfile, try to get the name of the album & artist
+    # not possible if the user renamed it so there's a fallback
+    # zipfiles from bandcamp have the following structure: "{ARTIST} - {ALBUM}.zip"
+    try:
+        artistname, albumname = i.split(' - ')
+    except ValueError:
+        artistname = ''
+        albumname = i
+
+    # create tempdir, if it already exists delete it and retry
+    tempdir = Path(f'{albumname}-temp')
+    if tempdir.exists():
+        shutil.rmtree(tempdir)
+    tempdir.mkdir()
+    zip_ref.extractall(tempdir)
+
+    if args.verbose == True:
+        print(f'Extracted: {i+'.zip'}')
+
+    zip_ref.close()
+
+    if args.clean == True:
+        Path(i+'.zip').unlink()
+        if args.verbose == True:
+            print(f'Deleted {i+'.zip'}')
+
+    finaldir = Path(f'{albumname}')
+    if finaldir.exists():
+        print(f'ERROR: Cannot create directory \'{albumname}\' as directory already exists', file=sys.stderr)
+        if args.keep == False:
+            shutil.rmtree(tempdir)
+        exit(1)
+    else:
+        finaldir.mkdir()
+
+    for file in tempdir.iterdir():
+        if file.suffix in MUSICFILETYPES:
+            tagger = music_tag.load_file(file)
+            newname = f'{str(tagger.resolve('tracknumber')).zfill(2)} {tagger.resolve('tracktitle')}{file.suffix}' # i have no fucking clue why but STFU PYRIGHT
+        else:
+            nameelements = file.name.split(' - ')
+            newname = nameelements[-1]
+
+        newfile = finaldir / newname
+        shutil.copy(tempdir / file.name, newfile)
+
+        if args.verbose == True:
+            print(f'Copied \'{file}\' to \'{newfile}\'.')
+
+    if args.keep == False:
+        shutil.rmtree(tempdir)
+        if args.verbose == True:
+            print(f'Deleted \'{tempdir}\'')
